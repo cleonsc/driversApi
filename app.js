90f9617286832d76fb050224b05d431892fc4bba
César León Schaus <cschaus@teceng-gaming.com.ar>
2020-12-13 11:52:00 -0300

    [edit]Ajustes y code clean en app.js para poder testear

--- app.js
+++ app.js
@@ -5,23 +5,13 @@ const swaggerExpressBootstrap = require('./api/bootstrap/swaggerExpress.bootstra
 const MODULE_NAME = '[Main App]';
 const appRoot = __dirname;
 
-
-function logAppStarted(functionName) {
-  log.info(`${MODULE_NAME} ${functionName}`);
-  log.info(`${MODULE_NAME} ${functionName} -------------------------------------------------------------------------`);
-  log.info(`${MODULE_NAME} ${functionName} --                         Drivers App Initialized OK!                         --`);
-  log.info(`${MODULE_NAME} ${functionName} -------------------------------------------------------------------------`);
-}
-
 async function init() {
   try {
     log.info(`${MODULE_NAME}:${init.name} (IN) --> starting}`);
 
     await swaggerExpressBootstrap.start(appRoot, process.env.NODE_PORT);
 
-    logAppStarted(init.name);
-
-    return true;
+    return swaggerExpressBootstrap.server;
   } catch (error) {
     log.error(`${MODULE_NAME}:${init.name} (ERROR) --> error: ${error.message}`);
     return false;
@@ -32,11 +22,11 @@ function stop() {
   swaggerExpressBootstrap.stop();
 }
 
-// The application starts here when this module is loaded!
 init();
 
 module.exports = {
   init,
   stop,
+  log
 };
 
